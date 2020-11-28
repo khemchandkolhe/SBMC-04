c7d3c83a648435539a09afab1a498ee33406708e
khemchandkolhe <khemchandkolhe3@gmail.com>
2020-11-27 17:08:47 -0800

    sop added

--- src/main/java/com/ashokit/PhoneBookAppApplication.java
+++ src/main/java/com/ashokit/PhoneBookAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PhoneBookAppApplication {
 
 	public static void main(String[] args) {
+		system.out.println();
 		SpringApplication.run(PhoneBookAppApplication.class, args);
 	}
 
